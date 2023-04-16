f65a7b792d8007244c02778644483938db8b01e1
Vladislav Aleksandrov <vladislav.aleksandrov1994@mail.ru>
2023-04-16 20:51:52 +0300

    fix

--- src/components/MyPosts.jsx
+++ src/components/MyPosts.jsx
@@ -23,7 +23,9 @@ export const MyPosts = () => {
               id={obj._id}
               title={obj.title}
               imageUrl={
-                obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ""
+                obj.imageUrl
+                  ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}`
+                  : ""
               }
               user={obj.user}
               createdAt={obj.createdAt}
